f73a5c5cdee577f3584e813c1f3ea50543e1e2cc
hussam <chaudhry.hussam@gmail.com>
2015-06-20 19:18:39 +0200

    Addes label for seach spinner and verification of internet connection

--- src/fr/esgi/findadesk/HomeActivity.java
+++ src/fr/esgi/findadesk/HomeActivity.java
@@ -1,15 +1,20 @@
 package fr.esgi.findadesk;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends ActionBarActivity{
 
 	private Button findLocation;
 	private Button myReservations;
+	private boolean isInternetOn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,13 @@ public class HomeActivity extends ActionBarActivity{
 
 		findLocation = (Button) findViewById(R.id.find_loc_btn);
 		myReservations = (Button) findViewById(R.id.reservations_btn);
+		
+		isInternetOn = isInternetOn();
+		
+		if(isInternetOn == false)
+		{
+			showDialog();
+		}
 
 		findLocation.setOnClickListener(new View.OnClickListener() {
 
@@ -29,5 +41,39 @@ public class HomeActivity extends ActionBarActivity{
 			}
 		});
 
+	}
+	
+	public final boolean isInternetOn() {
+
+		ConnectivityManager connec = (ConnectivityManager) getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+		
+		if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED
+				|| connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING
+				|| connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING
+				|| connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED) {
+			return true;
+
+		} else if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED
+				|| connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED) {
+			return false;
+		}
+		return false;
+	}
+	
+	private void showDialog() {
+		
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(HomeActivity.this);
+	     
+		 alertDialogBuilder.setTitle("Erreur");
+		 alertDialogBuilder.setMessage("Pas de connexion Internet !");
+		
+		 alertDialogBuilder.setNeutralButton("Quitter",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+					HomeActivity.this.finish();
+				}
+			});
+		 
+		 AlertDialog alertDialog = alertDialogBuilder.create();
+		 alertDialog.show();
 	}
 }
